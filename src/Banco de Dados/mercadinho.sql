ca489e4727fc591b5239ac2a8a7bc62053f401d0
Tiago de Oliveira <tiagozero3@gmail.com>
2021-11-14 15:43:33 -0300

    Adicionado tela lista de compras e sistema de venda de produtos

--- src/Banco de Dados/mercadinho.sql
+++ src/Banco de Dados/mercadinho.sql
@@ -1,137 +1,49 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 04-Nov-2021 às 13:30
--- Versão do servidor: 10.4.21-MariaDB
--- versão do PHP: 8.0.10
+create DATABASE mercadinho;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+create table funcionarios (
+    id_funcionario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    usuario VARCHAR(50) NOT NULL,
+    senha VARCHAR(50) NOT NULL
+);
 
+create table produtos (
+    id_produto INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    valor DECIMAL(5,2) NOT NULL,
+    quantidade INT DEFAULT 0
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+create table lista_compras (
+    id_lista_compras INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_produto INT NOT NULL,
+    quantidade INT DEFAULT 1
+);
 
---
--- Banco de dados: `mercadinho`
---
-CREATE DATABASE IF NOT EXISTS `mercadinho` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `mercadinho`;
+create table vendas (
+    id_venda INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    valor DECIMAL(5,2) NOT NULL
+);
 
--- --------------------------------------------------------
+create table produtos_vendidos (
+    id_produtos_vendidos INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    id_venda INT NOT NULL,
+    id_produto INT NOT NULL,
+    quantidade INT DEFAULT 1
+);
 
---
--- Estrutura da tabela `caixas`
---
+create table fornecedores (
+    id_fornecedor INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50) NOT NULL,
+    telefone BIGINT(20) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    estado VARCHAR(50) DEFAULT NULL,
+    cidade VARCHAR(50) DEFAULT NULL,
+    endereço VARCHAR(50) DEFAULT NULL
+);
 
-CREATE TABLE `caixas` (
-  `id` int(11) NOT NULL,
-  `usuario` varchar(50) NOT NULL,
-  `senha` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+INSERT INTO `produtos` (`id_produto`, `nome`, `valor`, `quantidade`) VALUES (NULL, 'Abacaxi', '3.99', '10');
+INSERT INTO `produtos` (`id_produto`, `nome`, `valor`, `quantidade`) VALUES (NULL, 'Limão', '1.99', '10');
+INSERT INTO `produtos` (`id_produto`, `nome`, `valor`, `quantidade`) VALUES (NULL, 'Miojo', '1.25', '10');
 
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `estoque`
---
-
-CREATE TABLE `estoque` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `quantidade` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `fornecedores`
---
-
-CREATE TABLE `fornecedores` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `telefone` bigint(20) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `estado` varchar(50) DEFAULT NULL,
-  `cidade` varchar(50) DEFAULT NULL,
-  `endereço` varchar(50) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `produtos`
---
-
-CREATE TABLE `produtos` (
-  `id` int(11) NOT NULL,
-  `nome` varchar(50) NOT NULL,
-  `valor` decimal(5,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Índices para tabelas despejadas
---
-
---
--- Índices para tabela `caixas`
---
-ALTER TABLE `caixas`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `estoque`
---
-ALTER TABLE `estoque`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `fornecedores`
---
-ALTER TABLE `fornecedores`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `produtos`
---
-ALTER TABLE `produtos`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT de tabelas despejadas
---
-
---
--- AUTO_INCREMENT de tabela `caixas`
---
-ALTER TABLE `caixas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `estoque`
---
-ALTER TABLE `estoque`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `fornecedores`
---
-ALTER TABLE `fornecedores`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT de tabela `produtos`
---
-ALTER TABLE `produtos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+INSERT INTO `lista_compras` (`id_lista_compras`, `id_produto`, `quantidade`) VALUES (NULL, '1', '1');
+INSERT INTO `lista_compras` (`id_lista_compras`, `id_produto`, `quantidade`) VALUES (NULL, '3', '2');
